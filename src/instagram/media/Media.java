82ec0f6d70f3909b79c06fe6f03f672a0a68f4d8
mangel <miguelvall2703@gmail.com>
2024-05-16 13:21:06 +0200

    Actualizados los ficheros con alguna implementacion más. Aún queda bastante por hacer.

--- src/instagram/media/Media.java
+++ src/instagram/media/Media.java
@@ -1,6 +1,57 @@
 package instagram.media;
-
+import javax.swing.*;
+import java.awt.*;
 import java.io.Serializable;
-
+import java.util.ArrayList;
 public class Media implements Serializable {
+    private String name;
+    private String internalName;
+    private int likes;
+    private boolean adultContent;
+    private ArrayList<String> comments;
+    private double score;
+    private int numVotes;
+    private ImageIcon cover;
+    public Media(String name) {
+        this.name = name;
+        this.internalName = name.trim().replaceAll("[^a-zA-Z0-9]+", "_").toLowerCase();
+        this.likes = 0;
+        this.adultContent = false;
+        this.comments = new ArrayList<>();
+        this.score = 0;
+        this.numVotes = 0;
+        this.cover = null;
+    }
+    public String getName(){ return this.name; }
+    public String getInternalName(){ return this.internalName; }
+    public int getLikes() { return this.likes; }
+    public boolean isAdultContent() {return this.adultContent; }
+    public ArrayList<String> getComments() {return this.comments; }
+    public double getScore() { return this.score; }
+    public ImageIcon getCover() { return this.cover; }
+    public void addLike() { this.likes++; }
+    public void tagAdultContent(boolean flag) { this.adultContent = flag; }
+    public void addComment(String comment) { this.comments.add(comment); }
+    public void addScore(double sc) { score = ( (score * numVotes) + sc) / (++numVotes); }
+    public void setCover(ImageIcon img) { this.cover = img; }
+    public String toString() {
+        showCover();
+        String msg = "\n\n© "+this.name+(this.adultContent ? " [+18]" : " [AP (all public)]")+".";
+        msg += "\n \u2606"+" "+Math.round(this.score*100)/100.0+" ("+this.numVotes+" votes)";
+        msg += "\n \uD83D\uDC4D"+" "+this.likes;
+        msg += "\n "+this.comments.size()+" comments found on this media:";
+        for(int i=0; i<this.comments.size(); i++) msg += " \n #"+i+": "+this.comments.get(i)+".";
+        return msg;
+    }
+    public void showCover() {
+        if (cover == null) return;
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        frame.add((new JPanel()).add(new JLabel(getCover())));
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public void loadCover(String path) {
+        setCover(new ImageIcon(Toolkit.getDefaultToolkit().createImage(path)));
+    }
 }
--- src/instagram/rmi/common/InstagramServer.java
+++ src/instagram/rmi/common/InstagramServer.java
@@ -1,5 +1,38 @@
 package instagram.rmi.common;
 
-public class InstagramServer {
+import java.io.FileNotFoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+import instagram.media.Media;
+
+/**
+ * Interfaz remota que le sirve al cliente para ejecutar métodos del servidor (con un objeto remoto)
+ */
+public interface InstagramServer extends Remote {
+
+    /**
+     * Establece una conexión con un cliente. El objeto cliente que se recibe es remoto.
+     * @param client objeto remoto perteneciente a la conexión entrante
+     * @return {@code true} si se ha conectado sin problemas. {@code false} si no ha podido
+     * @throws RemoteException si ha ocurrido algún error durante la transimisión entre cliente y servidor
+     * @throws IllegalArgumentException si {@code client == null}
+     */
+    boolean setClientStreamReceptor(InstagramClient client) throws RemoteException;
+
+    /**
+     * Reproduce un media aleatorio en el cliente establecido mediante {@code setClientStreamReceptor()}
+     * @return un mensaje con información útil respecto al resultado de la operación.
+     * @throws RemoteException si ha ocurrido algún error durante la transmisión entre cliente y servidor
+     */
+    String randomPlay() throws RemoteException;
+
+    /**
+     * Empieza a reproducir el objeto Media en el cliente establecido mediante {@code setClientStreamReceptor()}
+     * @param media el contenido a reproducir.
+     * @return un mensaje con el resultado de la operación.
+     * @throws RemoteException si ha ocurrido un error en la transmisión de datos.
+     * @throws FileNotFoundException si el contenido no se encuentra en el sistema.
+     */
+    String startMedia(Media media) throws RemoteException, FileNotFoundException;
 }
