ee8afebd7a63b47f726f4e566c45f663f4b411ef
Edgar Santiesteban <edgar.santiesteban22@estudiantes.uva.es>
2024-05-17 17:55:51 +0200

    Añadida implementacion de la interfaz InstagramClient mediante la nueva clase InstagramClientImpl (todavia sin probar)

---
+++ src/instagram/rmi/client/stream/InstagramClientImpl.java
@@ -0,0 +1,39 @@
+package instagram.rmi.client.stream;
+
+import instagram.media.*;
+import instagram.media.MediaPlayer;
+import instagram.rmi.common.InstagramClient;
+import instagram.stream.ClientStream;
+
+import java.io.FileNotFoundException;
+import java.rmi.RemoteException;
+
+public class InstagramClientImpl implements InstagramClient {
+    private Thread playerThread;
+
+    @Override
+    public boolean launchMediaPlayer(Media media) throws RemoteException, FileNotFoundException {
+        try {
+            MediaPlayer mediaplayer = new MediaPlayer(
+                    Globals.player_command,
+                    Globals.player_abs_filepath+media.getName()+
+                            Globals.file_extension,
+                    Globals.player_delay_ms
+            );
+            playerThread = new Thread(mediaplayer, "mediaPlayer");
+            playerThread.start();
+        }catch (Exception e){ e.printStackTrace(); return false; }
+        return true;
+    }
+
+    @Override
+    public boolean isMediaPlayerActive() throws RemoteException {
+        return playerThread.isAlive();
+    }
+
+    @Override
+    public void startStream(Media transferedMedia, String ip, int port) throws RemoteException, FileNotFoundException {
+        ClientStream cs = new ClientStream(transferedMedia, ip, port, playerThread);
+        new Thread(cs, "clientstream").start();
+    }
+}
