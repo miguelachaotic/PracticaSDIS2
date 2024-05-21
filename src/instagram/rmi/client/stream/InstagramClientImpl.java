861724661dfbaa172b21582af31b1eb00c4e65ab
Miguel Ángel <119065677+miguelachaotic@users.noreply.github.com>
2024-05-21 21:15:21 +0200

    Update InstagramClientImpl.java

--- src/instagram/rmi/client/stream/InstagramClientImpl.java
+++ src/instagram/rmi/client/stream/InstagramClientImpl.java
@@ -4,37 +4,48 @@ import java.io.FileNotFoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
+import instagram.media.Globals;
 import instagram.media.Media;
+import instagram.media.MediaPlayer;
 import instagram.rmi.common.InstagramClient;
+import instagram.stream.ClientStream;
 
 public class InstagramClientImpl extends UnicastRemoteObject implements InstagramClient {
+
+    private Thread playerThread;
 
 
     public InstagramClientImpl() throws RemoteException {
         super();
-
-    }
-
-    public InstagramClientImpl(int port) throws RemoteException {
-        super(port);
-
     }
 
 
     @Override
     public boolean launchMediaPlayer(Media media) throws RemoteException, FileNotFoundException {
-        System.out.println(media);
-        return false;
+        try{
+            MediaPlayer mediaPlayer = new MediaPlayer(
+                    Globals.player_command,
+                    Globals.player_abs_filepath + media.getName() + Globals.file_extension,
+                    Globals.player_delay_ms
+            );
+            playerThread = new Thread(mediaPlayer, "mediaPlayer");
+            playerThread.start();
+
+            return true;
+        } catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
     public boolean isMediaPlayerActive() throws RemoteException {
-        System.out.println("isMediaPlayerActive");
-        return false;
+        return playerThread.isAlive();
     }
 
     @Override
     public void startStream(Media transferedMedia, String ip, int port) throws RemoteException, FileNotFoundException {
-        System.out.println("startStream");
+        ClientStream clientStream = new ClientStream(transferedMedia, ip, port, playerThread);
+        new Thread(clientStream, "clientStream").start();
     }
 }
