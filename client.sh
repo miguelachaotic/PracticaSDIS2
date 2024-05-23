9336a2057ef0f1b007482afca769ac44b2d642e7
miguel <miguelvall2703@gmail.com>
2024-05-23 15:37:35 +0200

    FUNCIONAL !!!!!!!!!!!!

--- client.sh
+++ client.sh
@@ -3,5 +3,5 @@ java -cp . -Djavax.net.ssl.trustStore=Client_Truststore.p12 \
   -Dfile.encoding=UTF-8 \
   -Djavax.net.ssl.keyStore=Server_Keystore.p12  \
   -Djavax.net.ssl.keyStorePassword=servidorpass \
-  -Djava.rmi.server.hostname=10.0.200.53 \ 
+  -Djava.rmi.server.hostname=10.0.200.53 \
   instagram.rmi.client.directory.unit.ClienteNormal
